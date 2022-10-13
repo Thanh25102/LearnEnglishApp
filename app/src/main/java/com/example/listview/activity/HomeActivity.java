3f32803e296ed29505c8c6eb6a258bf24a8b6b50
manhthanh <manhthanh147@gmail.com>
2022-10-13 14:09:04 +0700

    idk

---
+++ app/src/main/java/com/example/listview/activity/HomeActivity.java
@@ -0,0 +1,14 @@
+package com.example.listview.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.listview.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
---
+++ app/src/main/java/com/example/listview/activity/LessonActivity.java
@@ -0,0 +1,14 @@
+package com.example.listview.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.listview.R;
+
+public class LessonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lesson);
+    }
+}
