56bfe7ad5e59c39b7b0c6bbcc81606b783be3738
manhthanh <manhthanh147@gmail.com>
2022-10-20 21:06:30 +0700

    code view edit profile

---
+++ app/src/main/java/com/example/listview/ui/AccountActivity.java
@@ -0,0 +1,14 @@
+package com.example.listview.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.listview.R;
+
+public class AccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account);
+    }
+}
